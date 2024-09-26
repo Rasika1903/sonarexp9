7a8ba4a5869202229af742ca3697732fc573bbb2
Rasika1903 <harihar.rasika@gmail.com>
2024-09-26 10:13:49 +0530

    pos.py

---
+++ pos.py
@@ -0,0 +1,11 @@
+def check_positive(x):
+    if x > 0:
+        return True
+    else:
+        return False
+    print("This is unreachable")
+def process_data(data):
+    if data is not None:
+        if data["type"] == "text":
+            # multiple nested conditions
+            pass
